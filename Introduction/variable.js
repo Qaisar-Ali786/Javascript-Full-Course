a84b06d2880a085e8ab53bf23ea4bc22f9d8545e
Qaisar-Ali786 <qaisarcaptain9@gmail.com>
2026-01-26 02:08:48 -0800

    Variable

---
+++ Introduction/variable.js
@@ -0,0 +1,2 @@
+// What is variable
+// a quantity which value can be changed during the execution of program. A variable is a box where we can store our data and later if we want to reuse our data, or change it , so we can do it
