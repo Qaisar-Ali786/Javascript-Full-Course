d8f002d39cc20149ac7b7de24346e0a64c6dee38
Qaisar-Ali786 <qaisarcaptain9@gmail.com>
2026-01-26 01:53:51 -0800

    Intro

---
+++ Introduction/intro.js
@@ -0,0 +1,2 @@
+// What is Javascript ?
+// javascript is a versatile and widely used proramming language that play a crucial role in web development. it enable developer to create interactive and dynamic elements within web page and enhance user experience by adding functionalities that responds to the user action. It is also used in other environment like server-side- script (Node Js)
