74eeb89779dfa04180f376952878400053163120
Qaisar-Ali786 <qaisarcaptain9@gmail.com>
2026-01-27 00:23:57 -0800

    Naming convensions

---
+++ Introduction/05. namingConvension.js
@@ -0,0 +1,20 @@
+// NAMING CONVENSION
+
+// UpperCase Convension
+let NAME = "Qaisar";
+
+// LowerCase Convension
+
+let name = "Ali";
+
+// KababCase Convension
+
+let My_Name = "Ahmed";
+
+// CamelCase Convension
+
+let myName = "Qaisar";
+
+// SnakeCase Convesion
+
+let MynameIs = "Aqil";
