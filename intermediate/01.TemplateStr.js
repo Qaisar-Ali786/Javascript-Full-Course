04dfb4a8801cef15c31a54d030fd1ea753500bcc
Qaisar-Ali786 <qaisarcaptain9@gmail.com>
2026-01-30 22:50:09 -0800

    Template string

---
+++ intermediate/01.TemplateStr.js
@@ -0,0 +1,11 @@
+// TEMPLATE STRING
+// template string also known as template literals, is a feature in javascript that allow you to create a string with embadded expression.
+// (embadded mean insert something inside the string (like variable or expression ) using ${} this)
+// (embadded expression mean insert something inside the string so the inserted thing will be appeaar in final output)
+// They are denoted by back ticks `` instead of double or single quotation marks. template string provide a more concise and flexible way to construct a string espically when they evolve variable or expression
+
+// EXAMPLE
+// `Respected sir,
+// my name is qaisar ali and in am from multan basically form muzaffargarh
+//     Your Obediently;
+//     Qaisar Ali,                                       `;
