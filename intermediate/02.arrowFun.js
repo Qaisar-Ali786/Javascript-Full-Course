a693b16c49580e9ccdab1268b7a21e761ad9ed07
Qaisar-Ali786 <qaisarcaptain9@gmail.com>
2026-01-30 23:33:18 -0800

    Enhance object literals

---
+++ intermediate/02.arrowFun.js
@@ -0,0 +1,11 @@
+// Arrow Function (ECMASCript 6):
+// arrow function are also known as fat function are a concise or shorter way to define a function in javascript. They are introduced in ECMASCript 6, and provide a more compact syntax compare to traditional function expression.
+// EXAMPLE:
+
+// const greet = (username) => {
+//   return `hellow ${username}`;
+// };
+// console.log(greet("Qaisar"));
+
+// const get = (username) => `hellow ${username}`;
+// console.log(get("Ali"));
---
+++ intermediate/enhanceObjLiter.js
@@ -0,0 +1,40 @@
+// ENHANCE OBJECT LITERALS (ECMASCript)
+// enhance object literals are a set of enhancement to the syntax for defining objects in javascript. These enhancement make it convenient and concise to define objects properties and methods.
+// EXAMPLE:
+
+// const name = "Qaisar";
+// const last = "Ali";
+// const age = 21;
+
+// const data = {
+//   name,
+//   last,
+//   age,
+// };
+// console.log(data);
+
+// function user(name, age, work) {
+//   return {
+//     name,
+//     age,
+//     work,
+//   };
+// }
+
+// const res = user("Qaisar", 21, "Student");
+// console.log(res);
+
+// function get(name, age, work) {
+//   return {
+//     name,
+//     age,
+//     work,
+//     intro: function () {
+//       return `my name is ${name} age is ${age} and work is ${work}`;
+//     },
+//   };
+// }
+
+// const res = get("Qaisar", 32, "Business");
+// console.log(res);
+// console.log(res.intro());
