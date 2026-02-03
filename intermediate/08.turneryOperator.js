4d4be0a974077282aba305b7fb405dd19fa5d235
Qaisar-Ali786 <qaisarcaptain9@gmail.com>
2026-02-03 03:59:04 -0800

    Turnery operator && for in loop

---
+++ intermediate/08.turneryOperator.js
@@ -0,0 +1,7 @@
+// What is turnary Operator
+// turnery operator in javascript is a concise way to write a conditional statement. it is the only javascript operator that take three operands
+// a condition followed by a question mark
+// an expression to execute if the condition is true
+// after colon an expression to execute if the condition is false
+
+// condition ? expression : expression
